bf97e7c626f4892a1314ddd6541af62a51987042
tkd9714 <chunwei.liu@goldenf.co>
2024-07-19 10:38:14 +0800

    add vendorCode

--- src/Contract/XJServiceInterface.php
+++ src/Contract/XJServiceInterface.php
@@ -13,19 +13,21 @@ interface XJServiceInterface {
      * @param string $account
      * @param string $game_code
      * @param string $language
+     * @param string $vendor_code
      * @param string $back_url
      * @return mixed
      */
-    function gameLaunch(string $op_code, string $account, string $game_code, string $language, string $back_url = null);
+    function gameLaunch(string $op_code, string $account, string $game_code, string $language, string $vendor_code, string $back_url = null);
 
     /**
      * 遊戲詳情
      * @param string $op_code
      * @param string $account
      * @param string $bet_id
+     * @param string $vendor_code
      * @return mixed
      */
-    function gameDetail(string $op_code, string $account, string $bet_id);
+    function gameDetail(string $op_code, string $account, string $bet_id, string $vendor_code);
 
     /**
      * 抓取遊戲紀錄
@@ -33,53 +35,59 @@ interface XJServiceInterface {
      * @param string $op_code
      * @param integer $past_minutes
      * @param string $cache_key
+     * @param string $vendorCode
      * @return mixed
      */
-    function betLogGrabber(string $op_code, int $past_minutes, string $cache_key);
+    function betLogGrabber(string $op_code, int $past_minutes, string $cache_key, string $vendorCode);
 
     /**
      * 全營商 抓取遊戲紀錄
      *
      * @param integer $past_minutes
+     * @param string $vendorCode
      * @return mixed
      */
-    function betLogGrabberAll(int $past_minutes);
+    function betLogGrabberAll(int $past_minutes, string $vendorCode);
 
     /**
      * 取得錢包餘額
      *
      * @param string $op_code
      * @param string $account
+     * @param string $vendorCode
      * @return mixed
      */
-    function getBalance(string $op_code, string $account);
+    function getBalance(string $op_code, string $account, string $vendorCode);
 
     /**
      * 遊戲上分
      *
      * @param string $op_code
      * @param string $account
+     * @param string $vendorCode
      * @return mixed
      */
-    function gameTransferIn(string $op_code, string $account);
+    function gameTransferIn(string $op_code, string $account, string $vendorCode);
 
     /**
      * 遊戲下分
      * @param string $op_code
      * @param string $account
      * @param float $amount
+     * @param string $vendorCode
      * @return mixed
      */
-    function gameTransferOut(string $op_code, string $account, float $amount);
+    function gameTransferOut(string $op_code, string $account, float $amount, string $vendorCode);
 
     /**
      * 上/下分失敗 訂單檢核
      *
      * @param string $op_code
      * @param string $order_no
+     * @param string $vendorCode
      * @return mixed
      */
-    function orderFailCheck(string $op_code, string $order_no);
+    function orderFailCheck(string $op_code, string $order_no, string $vendorCode);
 
     /**
      * 會員帳號轉換(GF -> XJ)
